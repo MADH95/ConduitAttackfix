a7ad6f9e4514265ae04e74671288624db90a0b1a
unknown <Martin.A.D.Harvey@hotmail.co.uk>
2021-12-14 16:26:57 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+using DiskCardGame;
+
+using BepInEx;
+using BepInEx.Logging;
+
+using HarmonyLib;
+
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+
+namespace ProjectName;
+
+[BepInPlugin( PluginGuid, PluginName, PluginVersion )]
+public class Plugin : BaseUnityPlugin
+{
+	private const string PluginGuid = "MADH.inscryption.ConduitAbilityFix";
+	private const string PluginName = "ConduitAbilityFix";
+	private const string PluginVersion = "1.0.0";
+
+	internal static ManualLogSource Log;
+
+	private void Awake ()
+	{
+		Logger.LogInfo( $"Loaded {PluginName}!" );
+		Plugin.Log = base.Logger;
+
+		Harmony harmony = new(PluginGuid);
+		harmony.PatchAll();
+
+	}
+}
+
+[HarmonyPatch( typeof( PlayableCard ) )]
+internal static class PlayableCard_GetPassiveAttackBuffs
+{
+    [HarmonyILManipulator]
+    [HarmonyPatch( nameof( PlayableCard.GetPassiveAttackBuffs ) )]
+    private static void EnablePart1IL ( ILContext il )
+    {
+        ILCursor c = new(il);
+
+        ILLabel label = null;
+        c.GotoNext( MoveType.After,
+            x => x.MatchCallvirt( AccessTools.PropertyGetter( typeof( SaveFile ), nameof( SaveFile.IsPart2 ) ) ),
+            x => x.MatchBrtrue( out label )
+        );
+
+        c.Emit( OpCodes.Call, AccessTools.PropertyGetter( typeof( SaveManager ), nameof( SaveManager.SaveFile ) ) );
+        c.Emit( OpCodes.Callvirt, AccessTools.PropertyGetter( typeof( SaveFile ), nameof( SaveFile.IsPart1 ) ) );
+        c.Emit( OpCodes.Brtrue, label );
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Conduit Attack Fix
+Fix for attack conduits in act 1
+
+Huge thanks to Aaron for writing the IL code and to Blind, the Bound Demon for the icon <3
+
+This mod allows you to use the conduit buff sigils from act 2 & 3 in act 1.
+
+**Note:** This doesn't add the art for existing sigils, for that, install [SigilArtFix](https://inscryption.thunderstore.io/package/MADH95Mods/SigilArtPatch/)
